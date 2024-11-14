8f1b8461d2f0255cfa4a9f6553b87dfe84cb5e78
CeaslessVoid <crocamedelain@gmail.com>
2024-11-14 18:55:30 +1100

    123123

--- Source/CrocamedelianInsects/CrocamedelianInsects/Hediff_SwarmingInfection.cs
+++ Source/CrocamedelianInsects/CrocamedelianInsects/Hediff_SwarmingInfection.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RimWorld;
+using rjw;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ namespace CrocamedelianInsects
         private const float SeverityReductionOnSpread = 0.2f;
         private const float SpreadChance = 0.2f;
         private const int   SpreadTicks = 60000;
+        private const float SeverityThreshold = 0.4f;
+        private const float ChanceToBroken = 0.2f;
 
         private static readonly List<string> TargetBodyParts = new List<string>
         {
@@ -41,6 +45,13 @@ namespace CrocamedelianInsects
             if (this.pawn.IsHashIntervalTick(60000) && this.Severity >= 0.33)
             {
                 ApplyAphrodisiacHediff();
+                ApplySwarmInfectionThought();
+                SpawnMaggots();
+
+                if (this.Part.def.defName == "Genitals" && pawn.gender == Gender.Female && Rand.Chance(ChanceToBroken) && this.Severity >= SeverityThreshold)
+                {
+                    BreakWomb();
+                }
             }
         }
 
@@ -102,6 +113,50 @@ namespace CrocamedelianInsects
                 pawn.health.AddHediff(newUnhealthy);
             }
 
+        }
+
+        private void ApplySwarmInfectionThought()
+        {
+
+            
+            if (xxx.is_zoophile(pawn))
+            {
+                ThoughtDef thoughtDef = ThoughtDef.Named("ZoophileSwarmInfection");
+                pawn.needs.mood.thoughts.memories.TryGainMemory(thoughtDef);
+            }
+            else
+            {
+                ThoughtDef thoughtDef = ThoughtDef.Named("SwarmInfection");
+                pawn.needs.mood.thoughts.memories.TryGainMemory(thoughtDef);
+            }
+            
+        }
+
+        private void SpawnMaggots()
+        {
+            if (pawn == null || pawn.Map == null) return;
+
+            int maggotCount = Rand.RangeInclusive(1, 2);
+
+            for (int i = 0; i < maggotCount; i++)
+            {
+                Thing maggot = ThingMaker.MakeThing(ThingDef.Named("Maggot"));
+                GenPlace.TryPlaceThing(maggot, pawn.Position, pawn.Map, ThingPlaceMode.Near);
+            }
+
+            Thing cum = ThingMaker.MakeThing(ThingDef.Named("FilthInsectCum"));
+            GenPlace.TryPlaceThing(cum, pawn.Position, pawn.Map, ThingPlaceMode.Near);
+        }
+
+        private void BreakWomb()
+        {
+            HediffDef insectBrokenDef = HediffDef.Named("InsectBroken");
+
+            if (insectBrokenDef != null && !this.pawn.health.hediffSet.HasHediff(insectBrokenDef))
+            {
+                Hediff newHediff = HediffMaker.MakeHediff(insectBrokenDef, this.pawn);
+                this.pawn.health.AddHediff(newHediff);
+            }
         }
     }
 
---
+++ Source/CrocamedelianInsects/CrocamedelianInsects/InsectEggPatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rjw;
+using HarmonyLib;
+using Verse;
+
+namespace CrocamedelianInsects
+{
+    [HarmonyPatch(typeof(PregnancyHelper), "DoEgg")]
+    public static class Patch_PregnancyHelper_DoEgg
+    {
+        private const float InfectionChance = 1.0f;
+        private const float ChanceToBroken = 0.2f;
+        static bool Prefix(SexProps props)
+        {
+
+            if (Rand.Chance(InfectionChance) && !xxx.is_insect(props.partner))
+            { 
+                BodyPartRecord targetBodyPart       = Genital_Helper.get_genitalsBPR(props.pawn);
+                Predicate<Hediff> filterByGenitalia = hediff => hediff.Part == targetBodyPart;
+                BodyPartRecord partnerGenitals      = null;
+                HediffDef swarmingInfectionDef      = HediffDef.Named("SwarmingInfection");
+                HediffDef insectBrokenDef           = HediffDef.Named("InsectBroken");
+
+                if (props.sexType == xxx.rjwSextype.Anal)
+                    partnerGenitals = Genital_Helper.get_anusBPR(props.partner);
+                else if (props.sexType == xxx.rjwSextype.Oral)
+                    partnerGenitals = Genital_Helper.get_stomachBPR(props.partner);
+                else if (props.sexType == xxx.rjwSextype.DoublePenetration && Rand.Value > 0.5f && RJWPregnancySettings.insect_anal_pregnancy_enabled)
+                    partnerGenitals = Genital_Helper.get_anusBPR(props.partner);
+                else
+                    partnerGenitals = Genital_Helper.get_genitalsBPR(props.partner);
+
+                Hediff existingInfection = props.partner.health.hediffSet.GetFirstHediffOfDef(swarmingInfectionDef);
+                if (existingInfection == null)
+                {
+                    Hediff newInfection = HediffMaker.MakeHediff(swarmingInfectionDef, props.partner, partnerGenitals);
+                    props.partner.health.AddHediff(newInfection);
+                }
+
+                if (props.sexType == xxx.rjwSextype.Vaginal && Rand.Chance(ChanceToBroken) && !props.partner.health.hediffSet.HasHediff(insectBrokenDef))
+                {
+                    Hediff newHediff = HediffMaker.MakeHediff(insectBrokenDef, props.partner);
+                    props.partner.health.AddHediff(newHediff);
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
